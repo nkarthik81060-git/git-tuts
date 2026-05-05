6423085ff306bb64d19b540330fbeede5fc86d07
nkarthik81060-git <nkarthik.81060@gmail.com>
2026-05-05 17:45:06 +0530

    Create Main class with execution methods
    Add Main class with myMethod and main method

---
+++ student.java
@@ -0,0 +1,17 @@
+public class Main {
+  static void myMethod() {
+    System.out.println("I just got executed!");
+  }
+
+  public static void main(String[] args) {
+    myMethod();
+    System.out.println("Hi hello how are you ");
+    System.out.println("hi run the code which is usningto develop the code");
+    System.out.println("I just got executed!");
+    System.out.println("I just got executed!");
+    
+    
+    
+    
+  }
+}
